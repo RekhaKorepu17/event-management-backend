93c80239623af87aa950e0f01a3863e2ce5c1177
RekhaKorepu17 <rekha.korepu@everest.engineering>
2024-12-13 13:06:29 +0530

    Create registrations model to store the registrations

---
+++ src/models/registration.ts
@@ -0,0 +1,44 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "../dbConnection";
+import User from "./user";
+import Events from "./event";
+
+const Registration = sequelize.define('Registration', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true, 
+  },
+  userId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'Users',
+      key: 'id',      
+    },
+    onDelete: 'CASCADE', 
+  },
+  eventId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'Events', 
+      key: 'id',       
+    },
+    onDelete: 'CASCADE', 
+  },
+  registrationDate: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: DataTypes.NOW, 
+  },
+  status: {
+    type: DataTypes.ENUM('registered', 'cancelled'),
+  }
+}, {
+  tableName: 'registrations', 
+  timestamps: false,           
+});
+
+
+export default Registration;
