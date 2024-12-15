c8d00b255296f3cca24d85a099aed750fe82c19b
RekhaKorepu17 <rekha.korepu@everest.engineering>
2024-12-15 23:47:07 +0530

    Modify user model

--- src/models/user.ts
+++ src/models/user.ts
@@ -13,20 +13,13 @@ const User = sequelize.define(
     username: {
       type: DataTypes.STRING,
       allowNull: false,
-      unique: true,
     },
     password: {
       type: DataTypes.STRING(64),
-      validate: {
-        is: /^[a-zA-Z](?=.*[0-9])(?=.*[\W_]).+$/i,
-      },
     },
     email: {
       type: DataTypes.STRING,
       allowNull: false,
-      validate: {
-        is: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-      },
     },
     mobile: {
       type: DataTypes.STRING,
