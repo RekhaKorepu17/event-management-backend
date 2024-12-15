3aeb6245c7f00a28f36c90f0d8a04da83e0b4138
RekhaKorepu17 <rekha.korepu@everest.engineering>
2024-12-16 00:11:49 +0530

    Modify events model

--- src/models/event.ts
+++ src/models/event.ts
@@ -1,59 +1,62 @@
 import { DataTypes } from "sequelize";
 import { sequelize } from "../dbConnection";
 
-const Event= sequelize.define('Events', {
+const Event = sequelize.define(
+  "Events",
+  {
     id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
     },
     name: {
-        type: DataTypes.STRING,
-        allowNull: false,
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     description: {
-        type: DataTypes.STRING,
+      type: DataTypes.STRING,
     },
-    Date: {
-        type: DataTypes.DATE,
-        allowNull: false,
-      },
-      startTime: {
-        type: DataTypes.DATE,
-        allowNull: false,
-      },
-      endTime: {
-        type: DataTypes.DATE,
-        allowNull: false,
-      },
-      address: {
-        type: DataTypes.STRING,  
-        allowNull: false,
-      },
-      eventType: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      eventStatus: {
-        type: DataTypes.ENUM('active', 'completed', 'cancelled'),
-        allowNull: false,
-        defaultValue: 'active',
-      },
-      organizerName: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      organizerContact: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      imageUrl: {
-        type: DataTypes.STRING,
-        allowNull: true,
-      },
-    },{
-    tableName: 'events', 
+    eventDate: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    startTime: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    endTime: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    eventType: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    eventStatus: {
+      type: DataTypes.ENUM("active", "completed", "cancelled"),
+      allowNull: false,
+      defaultValue: "active",
+    },
+    organizerName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    organizerContact: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    imageUrl: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+  },
+  {
+    tableName: "events",
     timestamps: false,
-    }
-)
+  }
+);
 export default Event;
