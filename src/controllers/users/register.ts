f2e8e30f353ac027132c455ce76ecb2dc4c78ca2
RekhaKorepu17 <rekha.korepu@everest.engineering>
2024-12-15 23:45:43 +0530

    Modify register controller to find user with email

--- src/controllers/users/register.ts
+++ src/controllers/users/register.ts
@@ -1,21 +1,29 @@
 import User from "../../models/user";
 import { User as UserType } from "../../types/User";
 import { User as UserClass } from "../../services/User";
-import {Request, Response} from "express"
+import { Request, Response } from "express";
 
- export const registerUser = async (req: Request, res: Response ) :Promise<any>=> {
-    try {
-      const {username, password, email, mobile, role}= req.body;
-      const newUser: UserType = new UserClass(username, password, email, mobile, role)
-      const existingUser:any = await User.findOne({ where: { username: username } });
-      if(existingUser){
-        return res.status(400).json({ message:"Username already exists"});
-      }
-      await User.create(newUser);
-      return res.status(400).json({ message:"User account created"});
-    } catch (error: any) {
-      return { message: "Failed to create a user", error: error.message };
+export const registerUser = async (
+  req: Request,
+  res: Response
+): Promise<any> => {
+  try {
+    const { username, password, email, mobile, role } = req.body;
+    const newUser: UserType = new UserClass(
+      username,
+      password,
+      email,
+      mobile,
+      role
+    );
+    const existingUser: any = await User.findOne({ where: { email: email } });
+    if (existingUser) {
+      return res.status(400).send({ message: "Email is already registered" });
     }
-  };
-
-
+    await User.create(newUser);
+    return res.status(201).send({ message: "User account created" });
+  } catch (error: any) {
+    console.error("Error during user registration:", error);
+    return { message: "Failed to create a user", error: error.message };
+  }
+};
