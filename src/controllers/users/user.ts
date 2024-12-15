25583969cdc09415fc748d8b6fa7cf49439249de
RekhaKorepu17 <rekha.korepu@everest.engineering>
2024-12-15 23:47:43 +0530

    Modify validate user functionality

--- src/controllers/users/user.ts
+++ src/controllers/users/user.ts
@@ -2,15 +2,15 @@ import {Request, Response} from "express"
 import User from "../../models/user";
 export const validateUser= async(req: Request, res: Response):Promise<any> => {
     try {
-        const { username, password } = req.query;
-        const user:any = await User.findOne({ where: { username: username } });
+        const { email, password } = req.query;
+        const user:any = await User.findOne({ where: { email: email } });
         if (!user) {
-          return res.status(404).json({ message:"User not found"});
+          return res.status(404).json({ message:"email not found"});
         }
         if (user.password !== password) {
-          return res.status(404).json({ message: "Invalid password" });
+          return res.status(401).json({ message: "Invalid password" });
         }
-        return res.status(201).json({ message: "Valid credentials" });
+        return res.status(200).json({ message: "Valid credentials", user: user});
       } catch (error: any) {
         return { message: "Failed to fetch credentials", error: error.message };
       }
