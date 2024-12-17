e98ca199f1e89c3ccaea7daa484bd4f5309516e2
RekhaKorepu17 <rekha.korepu@everest.engineering>
2024-12-17 12:10:10 +0530

    Modify post event to format the date

--- src/controllers/events/postEvent.ts
+++ src/controllers/events/postEvent.ts
@@ -2,26 +2,35 @@ import { Request, Response } from "express";
 import Event from "../../models/event";
 import { Event as EventClass } from "../../services/Event";
 
-export const postEvent = async (req: Request, res: Response): Promise<any>  => {
+export const postEvent = async (req: Request, res: Response): Promise<any> => {
   try {
     const { event } = req.body;
     const existingEvent: any = await Event.findOne({
       where: {
         name: event.name,
         organizerName: event.organizerName,
-        eventDate: event.eventDate,
+        eventDate: new Date(event.eventDate),
       },
     });
+
     if (existingEvent) {
       return res.status(409).json({ message: "Event already created" });
     }
-    const newEvent: any = new EventClass(event);
-    Event.create(newEvent);
+    const formattedEvent = {
+      ...event,
+      eventDate: new Date(event.eventDate),
+      startTime: new Date(`${event.eventDate}T${event.startTime}:00`),
+      endTime: new Date(`${event.eventDate}T${event.endTime}:00`),
+    };
+    const newEvent: any = new EventClass(formattedEvent);
+
+    const createdEvent = await Event.create(newEvent);
+    console.log('res', createdEvent);
     return res.status(201).json({ message: "Event created" });
   } catch (error: any) {
     return res.status(500).json({
-        message: "Failed to create event",
-        error: error.message,
-      });
+      message: "Failed to create event",
+      error: error.message,
+    });
   }
 };
