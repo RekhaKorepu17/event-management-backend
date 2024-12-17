6c8854c9863449331fea0386231400443adeb877
RekhaKorepu17 <rekha.korepu@everest.engineering>
2024-12-17 12:11:28 +0530

    Modify event routes

--- src/routes/event.routes.ts
+++ src/routes/event.routes.ts
@@ -1,10 +1,10 @@
 import express from "express";
 const eventRoutes = express.Router();
 import { postEvent } from "../controllers/events/postEvent";
-import { getAllEvents } from "../controllers/events/event";
+import { fetchEvents } from "../controllers/events/event";
 
 eventRoutes.post("/event", postEvent);
 
-eventRoutes.get("/events", getAllEvents);
+eventRoutes.get("/events", fetchEvents);
 
 export default eventRoutes;
